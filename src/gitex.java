f09703c36a308e1f309782f389b7f11f8881684f
wmr <1209801246@qq.com>
2018-01-15 09:21:27 +0800

    吴明茹修改

--- src/gitex.java
+++ src/gitex.java
@@ -3,5 +3,7 @@ public class gitex {
         System.out.println("my git exercise");
         System.out.println("crew has modified!");
         System.out.println("crew has modified the bug!!!!");
+        System.out.println("吴明茹改了代码!!!!!!!!!");
+        System.out.println("@@@@哈哈哈@@@@@@");
     }
 }
